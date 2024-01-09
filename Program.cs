5889606e4873268d22b2186300e9c83bf42a4fc7
Fabíola Boldrin <fabiboldrin8@gmail.comgit config --global user.email fabiboldrin8@gmail.comgi clear>
2024-01-09 00:23:50 -0300

    feito!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "Modelo Axx", imei: "748349", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "54321", modelo: "Modelo I4", imei: "99239", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
